7ba7eb98648a2ead7e0a576dbcc8f5b7c62222ca
ankitsah02 <ankitsah6898@gmail.com>
2026-02-02 12:30:43 +0530

    Add files via upload

---
+++ Day2_q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, pos;
+
+    // Input size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    scanf("%d", &pos);
+
+    for(int i = pos - 1; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
