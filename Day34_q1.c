b2d2d45235c579d705f7af603971d5df18775cfa
ankitsah02 <ankitsah6898@gmail.com>
2026-03-07 20:50:45 +0530

    Add files via upload

---
+++ Day34_q1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));x
+    newNode->data = x;
+    newNode->next = top;
+    top = newNode;
+}
+
+int pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    struct Node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int evaluate(char *exp) {
+    char *token = strtok(exp, " ");
+
+    while (token != NULL) {
+
+        if (isdigit(token[0])) {
+            push(atoi(token));
+        }
+        else {
+            int b = pop();
+            int a = pop();
+            int result;
+
+            switch(token[0]) {
+                case '+': result = a + b; break;
+                case '-': result = a - b; break;
+                case '*': result = a * b; break;
+                case '/': result = a / b; break;
+            }
+
+            push(result);
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    return pop();
+}
+
+int main() {
+    char exp[100];
+
+    fgets(exp, sizeof(exp), stdin);
+
+    int result = evaluate(exp);
+
+    printf("%d", result);
+
+    return 0;
+}
